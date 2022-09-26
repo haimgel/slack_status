a26f5ed9e400a8874d904b1ac0192d19bf04b099
Haim Gelfenbeyn <haim@g8n.me>
2022-09-26 14:02:57 -0400

    Dependencies update

--- src/settings.rs
+++ src/settings.rs
@@ -12,9 +12,11 @@ impl AppSettings {
     pub fn new() -> Result<Self, config::ConfigError> {
         let mut settings_path = home_dir().unwrap();
         settings_path.push(".slack_status");
-        let mut cfg = config::Config::default();
-        cfg.merge(config::File::from(settings_path))?
-            .merge(config::Environment::with_prefix("APP"))?;
-        cfg.try_into()
+
+        let builder = config::Config::builder()
+            .add_source(config::File::from(settings_path))
+            .add_source(config::Environment::with_prefix("APP"));
+
+        builder.build()?.try_deserialize()
     }
 }
